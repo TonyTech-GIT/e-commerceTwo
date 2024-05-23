0b8e3a2c4bd912c1b8c5223e19ca2e687c81b035
TonyTech <toomgbebu@gmail.com>
2024-05-23 21:57:36 +0100

    progress on stripe payment action

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,16 +1,36 @@
 "use client";
 
+import { userOrderExists } from "@/app/actions/orders";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { formatCurrency } from "@/lib/formatters";
 import {
   Elements,
+  LinkAuthenticationElement,
   PaymentElement,
   useElements,
   useStripe,
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import Image from "next/image";
+import { FormEvent, useState } from "react";
 import Stripe from "stripe";
 
 type checkoutFomrProps = {
-  product: {};
+  product: {
+    imagePath: string;
+    name: string;
+    priceInCents: number;
+    description: string;
+    id: string;
+  };
   clientSecret: string;
 };
 
@@ -23,15 +43,115 @@ export default function CheckoutForm({
   clientSecret,
 }: checkoutFomrProps) {
   return (
-    <Elements options={{ clientSecret }} stripe={stripePromise}>
-      <Form />
-    </Elements>
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+      <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+          <Image
+            src={product.imagePath}
+            fill
+            alt={product.name}
+            className="object-cover"
+          />
+        </div>
+
+        <div className="text-white">
+          <div className="text-lg">
+            {formatCurrency(product.priceInCents / 100)}
+          </div>
+          <h1 className="text-2xl font-bold">{product.name}</h1>
+          <div className="line-clamp-3 text-muted-foreground">
+            {product.description}
+          </div>
+        </div>
+      </div>
+      <Elements options={{ clientSecret }} stripe={stripePromise}>
+        <Form priceInCents={product.priceInCents} productId={product.id} />
+      </Elements>
+    </div>
   );
 }
 
-function Form() {
+function Form({
+  priceInCents,
+  productId,
+}: {
+  priceInCents: number;
+  productId: string;
+}) {
   const stripe = useStripe();
   const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMesage, setErrorMessage] = useState<string>();
+  const [email, setEmail] = useState<string>();
 
-  return <PaymentElement />;
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (stripe == null || elements == null || email == null) return;
+
+    setIsLoading(true);
+
+    // Check for existing order...
+    const orderExists = await userOrderExists(email, productId);
+
+    if (orderExists) {
+      setErrorMessage(
+        "You have already purchased this product. Try downloading it from the My Orders page"
+      );
+
+      setIsLoading(false);
+      return;
+    }
+
+    stripe
+      .confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success `,
+        },
+      })
+      .then(({ error }) => {
+        if (error.type === "card_error" || error.type === "validation_error") {
+          setErrorMessage(error.message);
+        } else {
+          setErrorMessage("An unknown error occurred.");
+        }
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Checkout</CardTitle>
+          {errorMesage && (
+            <CardDescription className="text-destructive">
+              {errorMesage}
+            </CardDescription>
+          )}
+        </CardHeader>
+
+        <CardContent>
+          <PaymentElement />
+
+          <div className="mt-4">
+            <LinkAuthenticationElement onChange={(e) => e.value.email} />
+          </div>
+        </CardContent>
+
+        <CardFooter>
+          <Button
+            className="w-full"
+            size="lg"
+            disabled={stripe == null || elements == null || isLoading}
+          >
+            {isLoading
+              ? "Purchasing..."
+              : ` Purchase - ${formatCurrency(priceInCents / 100)} `}
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
 }
